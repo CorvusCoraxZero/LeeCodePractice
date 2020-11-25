fe3a842069527b8947d3e2dd5aed10fe599b2e11
CorvusCoraxZero <3099725072@qq.com>
2020-11-25 16:49:51 +0800

    解出第21题 使用了指针 还存在递归的解法

--- src/AHNU/learning/data_structure/Question_0021.java
+++ src/AHNU/learning/data_structure/Question_0021.java
@@ -91,4 +91,20 @@ public class Question_0021 {
         }
     }
 
+    // 递归解法
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists2(l1, l2.next);
+            return l2;
+        }
+
+    }
+
 }
