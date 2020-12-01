98660898d34a3f6b47d99aab528b8d67b9590037
CorvusCoraxZero <3099725072@qq.com>
2020-12-01 21:39:52 +0800

    更新了工具类

--- src/AHNU/learning/data_structure/Util.java
+++ src/AHNU/learning/data_structure/Util.java
@@ -3,25 +3,75 @@ package AHNU.learning.data_structure;
 import java.util.Scanner;
 
 public class Util {
-    public static ListNode createNewListNode(){
+    // 创建一个新的链表节点
+    protected static ListNode createNewListNode() {
         Scanner sc = new Scanner(System.in);
-        ListNode node = new ListNode(),head = node;
+        ListNode node = new ListNode(), head = node;
         int input;
-        while (true){
+        while (true) {
             input = sc.nextInt();
-            if (input != -1){
+            if (input != -1) {
                 node.next = new ListNode(input);
                 node = node.next;
-            }else break;
+            } else break;
         }
         return head.next;
     }
 
-    public static void printListNode(ListNode list){
-        while (list != null){
-            System.out.print(list.val+" ");
+    // 打印整个链表
+    protected static void printListNode(ListNode list) {
+        while (list != null) {
+            System.out.print(list.val + " ");
             list = list.next;
         }
         System.out.println();
+    }
+
+    // 二分查找 在一个升序的数组里
+    protected static int binarySearch(int[] nums, int target, int left, int right) {
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;  // 据说这是一个防止溢出技巧
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
+        return -1;//找不到返回-1
+    }
+
+    // 寻找左边界的二分查找  其更为本质的思想可以理解为寻找比target小的数有几个
+    protected static int leftBoundBinarySearch(int[] nums, int target, int left, int right) {
+        int mid;
+        while (left < right) {   // left为要返回的坐标 当left == right时就会弹出
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+
+    // 寻找左边界的二分查找  其更为本质的思想可以理解为寻找比target小的数有几个
+    protected static int rightBoundBinarySearch(int[] nums, int target, int left, int right) {
+        int mid;
+        while (left < right) {   // left为要返回的坐标 当left == right时就会弹出
+            mid = left + (right - left + 1) / 2;  // 地板除的结果改为天花板除法
+            if (nums[mid] == target) {
+                left = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return nums[right] == target ? right : -1;
     }
 }
