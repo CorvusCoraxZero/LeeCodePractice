2bd84c8c2f0044540bce8a570243d9de4061aef1
CorvusCoraxZero <3099725072@qq.com>
2020-12-03 21:28:52 +0800

    解决了第48题 逐层旋转 这是官方解题 思路一样 没有时间了就没有写

--- src/AHNU/learning/data_structure/Question_0048.java
+++ src/AHNU/learning/data_structure/Question_0048.java
@@ -21,29 +21,35 @@ public class Question_0048 {
     public static void main(String[] args) {
         Question_0048 q = new Question_0048();
 
+        int[][] x = new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        q.rotate(x);
+        for (int[] ints : x) {
+            for (int anInt : ints) {
+                System.out.printf("%4d",anInt);
+            }
+            System.out.println("");
+        }
     }
 
     // 逐层旋转  这是官方解题 思路一样 没有时间了就没有写
-    class Solution {
-        public void rotate(int[][] matrix) {
-            int n = matrix.length;
-            for (int i = 0; i < n / 2 + n % 2; i++) {
-                for (int j = 0; j < n / 2; j++) {
-                    int[] tmp = new int[4];
-                    int row = i;
-                    int col = j;
-                    for (int k = 0; k < 4; k++) {
-                        tmp[k] = matrix[row][col];
-                        int x = row;
-                        row = col;
-                        col = n - 1 - x;
-                    }
-                    for (int k = 0; k < 4; k++) {
-                        matrix[row][col] = tmp[(k + 3) % 4];
-                        int x = row;
-                        row = col;
-                        col = n - 1 - x;
-                    }
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n / 2 + n % 2; i++) {  // 将矩阵左上角的1/2 * 1/2 内的元素挨个交换位置
+            for (int j = 0; j < n / 2; j++) {
+                int[] tmp = new int[4];
+                int row = i;
+                int col = j;
+                for (int k = 0; k < 4; k++) {  // 记录四个角的情况 即应该填入的数字（其实可以使用一个temp记录上一个的数字 直接填入 就不用循环两遍了）
+                    tmp[k] = matrix[row][col];
+                    int x = row;
+                    row = col;
+                    col = n - 1 - x;
+                }
+                for (int k = 0; k < 4; k++) {
+                    matrix[row][col] = tmp[(k + 3) % 4];   // 填入
+                    int x = row;
+                    row = col;
+                    col = n - 1 - x;
                 }
             }
         }
