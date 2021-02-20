340bffc30a415daeee29833b04358983b5815f85
Corvus_Corax_zero <309972507@qq.com>
2021-02-21 02:09:07 +0800

    更新了一个静态方法 用于获取树

--- src/AHNU/learning/data_structure/TreeNode.java
+++ src/AHNU/learning/data_structure/TreeNode.java
@@ -1,6 +1,8 @@
 package AHNU.learning.data_structure;
 
+import java.util.ArrayDeque;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class TreeNode {
     int val;
@@ -18,5 +20,35 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static TreeNode getTree(){
+        Scanner sc = new Scanner(System.in);
+        TreeNode root = new TreeNode(Integer.parseInt(sc.next()));
+        ArrayDeque<TreeNode> stack = new ArrayDeque<>();
+        stack.add(root);
+        String n;
+        while (true){
+            TreeNode t = stack.pop();
+            n = sc.next();
+            if (n.equals("#")){
+                return root;
+            }else if (n.equals("-")){
+                t.left = null;
+            }else{
+                t.left = new TreeNode(Integer.parseInt(n));
+                stack.add(t.left);
+            }
+            n = sc.next();
+            if (n.equals("#")){
+                return root;
+            }else if (n.equals("-")){
+                t.right = null;
+            }else{
+                t.right = new TreeNode(Integer.parseInt(n));
+                stack.add(t.right);
+            }
+
+        }
     }
 }
