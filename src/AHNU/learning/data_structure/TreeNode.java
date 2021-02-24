392d515c3cc5f463ac530ddc3a54ee75544e7179
Corvus_Corax_zero <309972507@qq.com>
2021-02-25 02:16:24 +0800

    解决了第105题 使用递归解法 并且利用hashMap提升查询速率

--- src/AHNU/learning/data_structure/TreeNode.java
+++ src/AHNU/learning/data_structure/TreeNode.java
@@ -1,8 +1,6 @@
 package AHNU.learning.data_structure;
 
-import java.util.ArrayDeque;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class TreeNode {
     int val;
@@ -48,7 +46,26 @@ public class TreeNode {
                 t.right = new TreeNode(Integer.parseInt(n));
                 stack.add(t.right);
             }
-
         }
+    }
+
+    /**
+     * 中序遍历
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> stk = new ArrayDeque<>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {  // 如果root不为空就把左节点一路遍历到底 并且把节点存入栈中
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop(); // 此时节点为null pop一个栈中的节点作为根节点 并把节点数据存入list
+            res.add(root.val);
+            root = root.right; // 看此时的节点有没有右节点
+        }
+        return res;
     }
 }
