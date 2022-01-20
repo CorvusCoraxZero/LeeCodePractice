65fa8c144f097ed33d7283ed0e3ff89fecdf20a0
The_zero <30996207@qq.com>
2022-01-20 16:43:54 +0800

    解决了 1449题  这是一个完全背包问题 使用动态规划解决
       // 这是一个完全背包问题 所以可一套用一个完全背包问题的状态转移方程
        // dp[i][j]表示前i个元素, 恰好构成成本为j时, 构成的最大的整数(整数用字符串表示, 无效状态用'#'表示)
        // dp[i][j] = std::max(dp[i - 1][j], dp[i][j - cost[i]] + value[i])
        // dp[i - 1][j]: 表示第i件物品选0个
        // dp[i][j - cost[i]] + value[i]: 表示第i件物品至少选一个

--- src/AHNU/learning/data_structure/Question_1449.java
+++ src/AHNU/learning/data_structure/Question_1449.java
@@ -1,9 +1,9 @@
 package AHNU.learning.data_structure;
 
 /*
-    给你一个整数数组 cost 和一个整数 target 。请你返回满足如下规则可以得到的 最大 整数：
-    给当前结果添加一个数位（i + 1）的成本为 cost[i] （cost 数组下标从 0 开始）。
-    总成本必须恰好等于 target 。
+    给你一个整数数组cost和一个整数target。请你返回满足如下规则可以得到的最大整数：
+    给当前结果添加一个数位（i + 1）的成本为cost[i]（cost数组下标从 0 开始）。
+    总成本必须恰好等于target。
     添加的数位中没有数字 0 。
     由于答案可能会很大，请你以字符串形式返回。
     如果按照上述要求无法得到任何整数，请你返回 "0" 。
@@ -41,19 +41,111 @@ package AHNU.learning.data_structure;
     链接：https://leetcode-cn.com/problems/form-largest-integer-with-digits-that-add-up-to-target
 */
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Question_1449 {
 
     public static void main(String[] args) {
         Question_1449 q = new Question_1449();
-        int[] nums = new int[]{1, 1, 1, 1, 1};
-        System.out.println(q.largestNumber(nums, 3));
+        int[] nums = new int[]{4, 3, 2, 5, 6, 7, 2, 5, 5};
+        System.out.println(q.largestNumber(nums, 9));
     }
 
+    // 这是一个完全背包问题 所以可一套用一个完全背包问题的状态转移方程
+    // dp[i][j]表示前i个元素, 恰好构成成本为j时, 构成的最大的整数(整数用字符串表示, 无效状态用'#'表示)
+    // dp[i][j] = std::max(dp[i - 1][j], dp[i][j - cost[i]] + value[i])
+    // dp[i - 1][j]: 表示第i件物品选0个
+    // dp[i][j - cost[i]] + value[i]: 表示第i件物品至少选一个
     public String largestNumber(int[] cost, int target) {
-        return "";
+        // 初始化
+        String[][] dp = new String[cost.length + 1][target + 1];
+
+        for (int i = 0; i < cost.length + 1; i++) {
+            dp[i][0] = "";
+        }
+
+        for (int i = 0; i < target; i++) {
+            dp[0][i] = "";
+        }
+
+        for (int i = 1; i < cost.length + 1; i++) {
+            if (cost[i - 1] <= target) {
+                dp[i][cost[i - 1]] = i + "";
+            }
+        }
+
+        // 遍历
+        for (int i = 1; i < cost.length + 1; i++) {
+            for (int j = 1; j < target + 1; j++) {
+                if (j - cost[i - 1] >= 0 && dp[i][j - cost[i - 1]] != null && !dp[i][j - cost[i - 1]].isEmpty()) {
+                    dp[i][j] = max(dp[i - 1][j], i + dp[i][j - cost[i - 1]]);
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j]);
+                }
+            }
+        }
+
+        if (dp[cost.length][target] == null || dp[cost.length][target].isEmpty()) {
+            return "0";
+        }
+        return dp[cost.length][target];
     }
+
+    private String max(String str1, String str2) {
+        if (str1 == null || str1.isEmpty()) {
+            return str2;
+        } else if (str2 == null || str2.isEmpty()) {
+            return str1;
+        }
+
+        if (str1.length() != str2.length()) {
+            if (str1.length() > str2.length()) {
+                return str1;
+            } else {
+                return str2;
+            }
+        }
+
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.charAt(i) > str2.charAt(i)) {
+                return str1;
+            } else if (str2.charAt(i) > str1.charAt(i)) {
+                return str2;
+            } else {
+                continue;
+            }
+        }
+
+        return str1;
+    }
+
+    // 根据题目要求 优化写法 将空与非法状态做区分 结构更清晰 思路更明确
+    public String largestNumber2(int[] cost, int target) {
+        String[][] dp = new String[10][target + 1];
+        Arrays.fill(dp[0], "#");
+        dp[0][0] = "";
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 0; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j - cost[i - 1] >= 0 && !dp[i][j - cost[i - 1]].equals("#")) {
+                    dp[i][j] = strMax(dp[i][j], i + dp[i][j - cost[i - 1]]);
+                }
+            }
+        }
+        return dp[9][target].equals("#") ? "0" : dp[9][target];
+    }
+
+    private String strMax(String a, String b) {
+        if (a.length() > b.length()) {
+            return a;
+        }
+        if (a.length() < b.length()) {
+            return b;
+        }
+        return a.compareTo(b) > 0 ? a : b;
+    }
+
 }
 
 
